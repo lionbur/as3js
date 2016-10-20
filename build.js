04a25e8757a09bf904eaecb5a4ac481583edba77
Lion Burger <lion@ps-equity.com>
2016-10-20 19:19:06 +0300

    -include end-to-end test while building: using original runtime.js -> using compiled/runtime.js -> using compiled/com/mcleodgaming/as3js/Main -> rebundled.js which should be identical to runtime.js

--- build.js
+++ build.js
@@ -3,29 +3,72 @@ var beautify = require('js-beautify').js_beautify;
 // Pull in loader library first
 global.AS3JS = require('./lib/as3.js');
 // Now Pull in the actual AS3JS program
-var AS3JS = require('./runtime.js');
+//var AS3JS = require('./compiled/com/mcleodgaming/as3js/Main');
 var path = require('path');
 var mkdirs = require('node-mkdirs');
-
-// Load the program
-var as3js = new AS3JS();
 
 var useStrict = true;
 var testCompiledPackages = false;
 
-// Execute the program 
-var result = as3js.compile({
-	srcPaths: ['./src'],
-	silent: false,
-	verbose: false,
-	safeRequire: !testCompiledPackages,
-	entry: 'com.mcleodgaming.as3js.Main',
-	entryMode: 'static',
-	supports: {
-		const: true,
-		let: true
-	}
-});
+var testCases = [
+	{
+		runtime: "./runtime.js",
+		bundle: "./compiled/runtime.js",
+		options: {
+			srcPaths: ['./src'],
+			silent: false,
+			verbose: false,
+			safefRequire: true,
+			entry: 'com.mcleodgaming.as3js.Main',
+			entryMode: 'static',
+			supports: {
+				const: true,
+				let: true,
+				accessors: true,
+				ImportJS: true
+			}
+		}
+	},
+	{
+		runtime: "./compiled/runtime.js",
+		outputPath: "./compiled/",
+		options: {
+			srcPaths: ['./src'],
+			silent: false,
+			verbose: false,
+			safeRequire: false,
+			entry: 'com.mcleodgaming.as3js.Main',
+			entryMode: 'static',
+			supports: {
+				const: true,
+				let: true,
+				accessors: true,
+				class: true,
+				super: true,
+//				static: true,
+				ImportJS: false
+			}
+		}
+	},
+	{
+		runtime: "./compiled/com/mcleodgaming/as3js/Main",
+		bundle: "./compiled/rebundled.js",
+		options: {
+			srcPaths: ['./src'],
+			silent: false,
+			verbose: false,
+			safeRequire: false,
+			entry: 'com.mcleodgaming.as3js.Main',
+			entryMode: 'static',
+			supports: {
+//				const: true,
+//				let: true,
+//				accessors: true,
+				ImportJS: true
+			}
+		}
+	},
+];
 
 function writeToSourceFile(filename, code) {
 	if (useStrict) {
@@ -33,7 +76,11 @@ function writeToSourceFile(filename, code) {
 	}
 	
 	mkdirs(path.dirname(filename));
-	return fs.writeFileSync(
+
+	if (fs.existsSync(filename)) {
+		fs.unlinkSync(filename);
+	}
+	fs.writeFileSync(
 		filename,
 		beautify(code, { indent_size: 2, max_preserve_newlines: 2 }),
 		"UTF-8",
@@ -41,27 +88,27 @@ function writeToSourceFile(filename, code) {
 	);
 }
 
-if (testCompiledPackages)
-{
-	var packages = result.packageSources;
-	var outputPath = './compiled/';
+for (var testCase of testCases) {
+var AS3JS = require(testCase.runtime);
+// Load the program
+var as3js = new AS3JS();
 
-	for (var fullClassName in packages)
+// Execute the program 
+var result = as3js.compile(testCase.options);
+
+if (testCase.bundle)
+{
+	// Output the resulting source code
+	console.log("Bundling " + Object.keys(result.packageSources).length + " packages to " + testCase.bundle);
+	writeToSourceFile(testCase.bundle, result.compiledSource);
+} else {
+	for (var fullClassName in result.packageSources)
 	{
-		var filename = path.join(outputPath, fullClassName.replace(/\./g, "/") + ".js");
+		var filename = path.join(testCase.outputPath, fullClassName.replace(/\./g, "/") + ".js");
 
 		console.log("Writing Class " + fullClassName + " to " + filename);
-		if (fs.existsSync(filename)) {
-			fs.unlinkSync(filename);
-		}
-		writeToSourceFile(filename, packages[fullClassName]);
+		writeToSourceFile(filename, result.packageSources[fullClassName]);
 	}
-} else {
-	var filename = "./compiled/runtime.js";
-	// Output the resulting source code
-	if (fs.existsSync(filename))
-	{
-		fs.unlinkSync(filename);
-	}
-	writeToSourceFile(filename, result.compiledSource);
+}
+
 }
